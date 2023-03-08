4c08b55f0f62e945d2df49ff5312024a849e6b7c
Chiichriis <chichris28@gmail.com>
2023-03-08 01:53:46 +0100

    new update

--- src/components/form/Register.jsx
+++ src/components/form/Register.jsx
@@ -1,125 +1,156 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import Login from './Login'
+
 
 const Register = () => {
-    const[action, setAction] = useState('Student')
+    const[action, setAction]= useState('Student')
+    const[login, setLogin] =useState(false)
   return (
-    <div className="w-full">
-      <div className="bg-rounded-sm flex flex-col justify-center items-center w-full">
-        <div  className="w-[300px] md:w-96 bg-white px-8 py-2 rounded-lg shadow-2xl">
-        <h1 className="text-center text-xl mb-4 font-bold">Register Here</h1>
-        <div className="flex justify-between mb-3">
-            <p className="border-black border-b-2 cursor-pointer" onClick={() => setAction('Student')}>Student</p>
-            <p className="border-black border-b-2 cursor-pointer" onClick={() =>setAction('Agent')}>Agent</p>
+    <>
+    <div className="flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-base pb-2 text-center items-center ml-3 pt-[10%] uppercase ">
+            Register Here
+          </h1>
         </div>
-            { action ==='Student'?(  <form action="" >
-          <div className="mb-3">
-            <label htmlFor="" className="block">
-              Name:
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Enter your name" 
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="" className="block">Email:</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="example@gmail.com"
-              id="email"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="" className="block">Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="**********"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="" className="block">Confirm Password:</label>
-            <input
-              type="password"
-              name="confirm password"
-              id="confirm password"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-3">
-            <button className="w-full bg-blue-500 text-white p-1 rounded-lg">Register</button>
-            <p className="text-center mt-2">Have an account? Login here</p>
-          </div>
-        </form>): (  <form action="" >
-          <div className="mb-2">
-            <label htmlFor="" className="block">
-              Name:
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Enter your name" 
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="" className="block">Email:</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="example@gmail.com"
-              id="email"
-              className=" p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="" className="block">Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="**********"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="" className="block">Confirm Password:</label>
-            <input
-              type="password"
-              name="confirm password"
-              id="confirm password"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="" className="block">Campus:</label>
-            <input
-              type="text"
-              name="Campus"
-              id="confirm password"
-              className="p-1 border border-blue-600 w-full rounded-lg"
-              placeholder="Gk capmus"
-            />
-          </div>
-          
-          <div className="mb-2">
-            <button className="w-full bg-blue-500 text-white p-1 rounded-lg">Register</button>
-            <p className="text-center mt-2">Have an account? Login here</p>
-          </div>
-        </form>)}
-      
+        <div className='flex items-center w-full justify-between p-2 px-4'>
+            <button onClick={()=>setAction("Student")} className='text-sm uppercase border-b py-1'>Student</button>
+            <button onClick={()=>setAction('Agent')} className='text-sm uppercase'>Agent</button>
         </div>
-      </div>
-    </div>
-  );
-};
+        </div>
 
-export default Register;
+        {
+            action==='Student'? (
+                <form action="" className="top-[5% px-4] w-full px-4 py-4">
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500 ">
+                    Full Name*
+                  </label>
+                  <input
+                    type="text"
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Email Address*
+                  </label>
+                  <input
+                    type="email"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Password*
+                  </label>
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Confirm Password*
+                  </label>
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Campus*
+                  </label>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <button className=" w-full pointer px-3 py-2.5 border border-blue-100 uppercase text-sm rounded text-slate-500 md:px-5 md:py-3 hover:bg-slate-100 transition-all duration-700 bg-blue-100">
+                  Register
+                </button>
+                <p className="text-sm pt-5 text-center mx-16">
+                  Have and account?{" "}
+                  {/* {
+                    login ===''
+                  } */}
+                  <Link to="Login" className="text-red-500 italic underline pointer">
+                    Login
+                  </Link>
+                </p>
+              </form>
+
+            ):(
+                <form action="" className="top-[5% px-4] w-full px-4 py-4 my-7">
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Full Name*
+                  </label>
+                  <input
+                    type="text"
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Email Address*
+                  </label>
+                  <input
+                    type="email"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Password*
+                  </label>
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="" className="block text-sm text-slate-500">
+                    Confirm Password*
+                  </label>
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    className="p-2 border bg-white rounded w-full"
+                  />
+                </div>
+                
+                <button className=" w-full pointer px-3 py-2.5 border border-blue-100 uppercase text-sm rounded text-slate-500 md:px-5 md:py-3 hover:bg-slate-100 transition-all duration-700 bg-blue-100">
+                  Register
+                </button>
+                <p className=" py-5 text-sm text-center mx-16">
+                  Have and account?{" "}
+                  <Link to="Login" className="text-red-500 italic underline pointer">
+                    Login
+                  </Link>
+                </p>
+              </form>
+            )
+        }
+
+       
+    </>
+  )
+}
+
+export default Register
